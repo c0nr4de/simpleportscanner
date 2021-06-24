df5ad0d749cc8683141c12f7a667ec1d58d67dc7
arctroopper <arctroopper@hotmail.com>
2021-06-24 04:13:28 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,11 +1,13 @@
 import socket
 
 while(True):
-    target = input("digite a url ou ip do seu alvo ")
-    port = input("digite a porta ")
+    target = input("digite a url ou ip do seu alvo: ")
+    port = input("digite a porta: ")
+    time = input("tempo maximo de espera em segundos: ")
 
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(int(time))
     codig = client.connect_ex((target,  int(port)))
 
     print (codig)
